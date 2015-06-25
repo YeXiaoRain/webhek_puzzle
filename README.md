edb9aebca2a5eb1781319b4fbcc06b744208eccb
YeXiaoRain <562178188@qq.com>
2015-06-25 22:33:39 +0800

    Add solver.c

--- README.md
+++ README.md
@@ -1,9 +1,48 @@
 # Blue Puzzle
 
 ## 简介
-这是一个简单的小游戏，非常容易上手，点击[这里](http://liudonghua123.github.io/webhek_puzzle/)开始吧！
+这是一个简单的小游戏，非常容易上手，点击[这里](http://yexiaorain.github.io/webhek_puzzle/)开始吧！
 
 ### 看一个简单的截图吧！
 
 ![image](raw/puzzle_preview.png)
+
+
+----
+
+# Solver
+
+Write by [YeXiaoRain](https://github.com/YeXiaoRain)
+
+## Usage
+
+**Step 1**
+
+Use your favorite editor to open `solver.c`
+
+Edit the GAMESIZE at line 2
+
+```c
+#define GAMESIZE 20
+```
+
+to the number you need too solve.
+
+**Step 2**
+
+compile it with `gcc -o solver solver.c`
+
+**Step 3**
+
+run it with `./solver`
+
+## About Solver
+
+For `n*n` Game
+
+Time complexity is `O(n^2 * 2^n)`
+
+Space complexity is `O(n^2)` which can reduce to `O(2*n)`
+
+It cost `4.1s` to solve `20*20` puzzle and cost `3.9s` if caculating and checking last line at the same time.
 
---
+++ solver.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<string.h>
+#define GAMESIZE 20
+long long tp[GAMESIZE+1]={0};
+long long twopower(int p){
+	if(tp[p])
+		return tp[p];
+	if(p%2)
+		return tp[p]=2*twopower(p/2)*twopower(p/2);
+	else
+		return tp[p]=twopower(p/2)*twopower(p/2);
+}
+int blockval(int box[][GAMESIZE],int i,int j){
+	int res=box[i][j];
+	if(i>0)
+		res^=box[i-1][j];
+	if(j>0)
+		res^=box[i][j-1];
+	if(j<GAMESIZE-1)
+		res^=box[i][j+1];
+	return res;
+}
+int tryi(long long val){
+	int gamebox[GAMESIZE][GAMESIZE];
+	memset(gamebox,0,sizeof(gamebox));
+	int i;	
+	//init
+	for(i=0;i<GAMESIZE;i++)
+		gamebox[0][i]=(val>>i)%2;
+	//fill
+	int j;
+	for(i=1;i<GAMESIZE;i++){
+		for(j=0;j<GAMESIZE;j++){
+			if(!blockval(gamebox,i-1,j)){
+				gamebox[i][j]=1;
+			}
+		}
+	}
+	//check
+	for(i=0;i<GAMESIZE;i++)
+		if(!blockval(gamebox,GAMESIZE-1,i))
+			return 0;
+	//print ans
+	printf("\n");
+	for(i=0;i<GAMESIZE;i++){
+		for(j=0;j<GAMESIZE;j++){
+			printf("%d ",gamebox[i][j]);
+		}
+		printf("\n");
+	}
+	return 1;
+}
+void printres(long long val){
+	int s=GAMESIZE;
+	printf("First line:\n");
+	while(s-->0){
+		printf("%d ",(int)(val%2));
+		val/=2;
+	}
+	printf("\n");
+	return ;
+}
+int main(){
+	tp[0]=1;
+	int i,maxi=twopower(GAMESIZE);
+	for(i=0;i<maxi;i++){
+		if(tryi(i))
+			break;
+		//printf("%d/%d\n",i,GAMESIZE);
+	}
+	printf("\n");
+	if(i==maxi)
+		printf("No solution\n");
+	else
+		printres(i);
+	return 0;
+}
